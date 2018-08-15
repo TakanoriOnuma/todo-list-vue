cae6de4afd009b999f2525fe97c43be9a7cf6811
onuma <ohnhistory@gmail.com>
2018-08-15 18:47:16 +0900

    vuexのロガーを入れる

--- src/javascripts/store/store.js
+++ src/javascripts/store/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createLogger from 'vuex/dist/logger';
 
 import todoList from './todoList/module';
 
@@ -8,5 +9,10 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   modules: {
     todoList
-  }
+  },
+  plugins: process.env.NODE_ENV !== 'production' ? [
+      createLogger({
+        collapsed: false
+      })
+    ] : []
 });
