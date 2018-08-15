23ad4c95f865b55c30e3a98d9fd6701e77d40b0a
onuma <ohnhistory@gmail.com>
2018-08-15 18:27:36 +0900

    月がずれている問題を修正

--- src/javascripts/components/TodoList.vue
+++ src/javascripts/components/TodoList.vue
@@ -30,7 +30,7 @@ export default {
      */
     formatDate(date) {
       const year = date.getFullYear();
-      const month = ('00' + date.getMonth()).slice(-2);
+      const month = ('00' + (date.getMonth() + 1)).slice(-2);
       const day = ('00' + date.getDate()).slice(-2);
       return `${year}-${month}-${day}`;
     }
